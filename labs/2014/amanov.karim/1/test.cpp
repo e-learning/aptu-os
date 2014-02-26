f267f10cf5e9f30bdecab8654e06df25990a14fe
Karim <karim@karama.(none)>
2014-02-26 18:29:38 +0400

    KarimAmanov HW1

---
+++ labs/2014/amanov.karim/1/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <sstream>
+#include <vector>
+int main(int argc, char* argv[]) {
+for(int i = 0; i < argc; i++) {
+	std::cout << argv[i] << std::endl;
+}
+return  0;
+}
+
