a0532c132d0af7b58cc13edf82b473cd7ee852e5
Tugulev.Oleg <olegoleg27@gmail.com>
2014-03-17 23:45:04 +0400

    complete kill

--- labs/1142/Tugulev.Oleg/1/main.cpp
+++ labs/1142/Tugulev.Oleg/1/main.cpp
@@ -4,12 +4,13 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 using namespace std;
-
 
 int main ()
 {
 string inp;
+
 while(1)
 {
 cout << ">";
@@ -40,15 +41,16 @@ switch(inp[0])
 					printf("%s\n", ent->d_name);
 				}
  
-			closedir(dir);
+			closedir(dir); 
 			
 			cout << "\n";
 			break;
+			
         }
     case 'p':
-		if(inp.compare(0,3,"pwd")==0)
+		if(inp.compare(0,3,"pwd")==0)  
 		{
-		  printf("Current dirrectory: %s \n\r",get_current_dir_name());
+		  printf("Current dirrectory: %s \n\r",get_current_dir_name()); 
 			cout << "\n";
 			break;
         }
@@ -58,6 +60,7 @@ switch(inp[0])
 			cout << "\n";
 			break;
 		}
+
     case 'k':       
 		if(inp.compare(0,4,"kill")==0)
 		{
@@ -66,10 +69,18 @@ switch(inp[0])
                         string pid=inp.substr(pid_pos_st+1,pid_pos_en-pid_pos_st-1);
 			size_t sig_pos=inp.find("-");
 			string sig=inp.substr(sig_pos+1,inp.length()-sig_pos);
-			printf("%s\n",pid.c_str());
-			printf("%s\n",sig.c_str());
-			cout << "\n";
-			break;
+			//printf("%s\n",pid.c_str());
+			//printf("%s\n",sig.c_str());
+			if (!kill(atoi(sig.c_str()),atoi(pid.c_str())))
+			  {
+			  printf("Can't kill this!\n");
+			  break;
+			  }
+			else
+			  {
+			    printf("Killed successfully\n");
+                            break;
+			  }
                 }
     case 'e':       
 		if(inp.compare(0,4,"exit")==0)
@@ -78,7 +89,7 @@ switch(inp[0])
 			cout << "\n";
 			exit(0);
 		}
-	default: 
+	default:  
 		{
 		printf("There's no such cmd :(");
 		cout << "\n";
