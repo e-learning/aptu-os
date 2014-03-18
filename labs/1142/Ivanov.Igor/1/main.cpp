314514d7ab027d9be54e079a4e68883b82e691ea
Ivanov.Igor <jonujkee@gmail.com>
2014-03-18 15:44:53 +0900

    ok

--- labs/1142/Ivanov.Igor/1/main.cpp
+++ labs/1142/Ivanov.Igor/1/main.cpp
@@ -1,6 +1,28 @@
+#include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <dirent.h>
+using namespace std;
 
-int main()
+int main(void)
 {
-	printf("Hello, World!\n");
+string command;
+while (1)
+{
+cout<<">";
+cin>>command;
+if(command=="ls")
+{
+DIR *Dir = opendir(".");
+if(!Dir) return 1;
+
+struct dirent *entry;
+while((entry=readdir(Dir))!=NULL)
+printf("%s\n", entry->d_name);
+closedir(Dir);
+}
+if(command=="exit") break;
+}
 }
