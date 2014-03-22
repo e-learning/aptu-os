b7ede62654c57c61d4ec2e9afd656487c2719248
Igor <jonujkee@gmail.com>
2014-03-22 15:13:47 +0400

    go

--- labs/1142/Ivanov.Igor/1/main.cpp
+++ labs/1142/Ivanov.Igor/1/main.cpp
@@ -8,51 +8,61 @@ using namespace std;
 
 int main(void)
 {
-	cout<<"Привет, "<<getenv("USERNAME")<<"!"<<endl;	
+        cout<<"Привет, "<<getenv("USERNAME")<<"!"<<endl;        
 
-	string command;
+        string command,argument;
+	char qstring[50];
+	char sep[10]=" ";
 	while (1)
 	{
-		cout<<">";
-		cin>>command;
-		
-		if(command=="help")
-		{
-			cout	<<"help - помощь"<<endl
-				<<"ls - вывод списка файлов в текущем каталоге"<<endl
-				<<"pwd - вывод полного имени текущего каталога"<<endl
-				<<"ps - вывод списка процессов"<<endl
-				<<"kill - отправка сигнала с заданным номером конкретному процессу"<<endl
-				<<"exit - выход"<<endl;
-		}
-		if(command=="ls")
-		{
-			DIR *Dir = opendir(".");
-			if(!Dir) return 1;
+	cout<<">";
+	cin>>qstring;
+	command=strtok(qstring,sep);
+               
+                if(command=="help")
+                {
+                        cout    <<"help - помощь"<<endl
+                                <<"ls - вывод списка файлов в текущем каталоге"<<endl
+                                <<"pwd - вывод полного имени текущего каталога"<<endl
+                                <<"ps - вывод списка процессов"<<endl
+                                <<"kill - отправка сигнала с заданным номером конкретному процессу"<<endl
+                                <<"exit - выход"<<endl;
+                }
+                if(command=="ls")
+                {
+                        DIR *Dir = opendir(".");
+                        if(!Dir) return 1;
 
-			struct dirent *entry;
-			while((entry=readdir(Dir))!=NULL)
-				cout<<entry->d_name<<endl;
-			closedir(Dir);
-		}
-		if(command=="pwd") cout<<getenv("PWD")<<endl;
+                        struct dirent *entry;
+                        while((entry=readdir(Dir))!=NULL)
+                                cout<<entry->d_name<<endl;
+                        closedir(Dir);
+                }
+                if(command=="pwd") cout<<getenv("PWD")<<endl;
 		if(command=="ps")
 		{
 			DIR *Dir = opendir("/proc");
 			if(!Dir) return 1;
-			
+
 			struct dirent *entry;
 			while((entry=readdir(Dir))!=NULL)
 			{
-				FILE *Inf=fopen(("/proc/"+entry->d_name+"/comm"),"r");
+				string InfName = "/proc/"+string(entry->d_name)+"/comm";
+				FILE *Inf=fopen(InfName.c_str(),"r");
 				if(Inf==NULL) return 1;
-				string name;
-				getline(Inf,name);
+				char name[40];
+				fgets(name,40,Inf);
 				cout<<entry->d_name<<" "<<name<<endl;
 				fclose(Inf);
 			}
+			closedir(Dir);
 		}
-				
-		if(command=="exit") break;
-	}
+		if(command=="kill")
+		{
+			argument=strtok(NULL,sep);
+			//kill(atoi(argument.c_str()),0);
+		}               
+                if(command=="exit") break;
+        }
 }
+
