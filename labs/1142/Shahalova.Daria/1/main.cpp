2096a9711c84e2573370076a0ce29049a7715e01
HTCTAC <HTCTAC@gmail.com>
2014-03-31 19:17:37 +0000

    main func fix

--- labs/1142/Shahalova.Daria/1/main.cpp
+++ labs/1142/Shahalova.Daria/1/main.cpp
@@ -1,6 +1,53 @@
-include<stdio.h>
-int main()
+int main(int argc, char** argv)
 {
-printf("My name is Vasya:\n");
-return 0;
+    string input_str, command, args;
+    
+    signal (SIGINT, sigint_handler);
+    
+    while(1)
+    {
+        cout << ">";
+        getline(cin, input_str);
+        if(parse_command(input_str, command, args) == 0)
+        {
+            if(command.compare("ls") == 0)
+            {
+                command_ls();
+                continue;
+            }
+            
+            if(command.compare("ps") == 0)
+            {
+                command_ps();
+                continue;
+            }
+            
+            if(command.compare("pwd") == 0)
+            {
+                command_pwd();
+                continue;
+            }
+            
+            if(command.compare("kill") == 0)
+            {
+                command_kill(args);
+                continue;
+            }
+            
+            if(command.compare("exit") == 0)
+            {
+                break;
+            }
+            
+            if(command.compare("help") == 0)
+            {
+                print_help();
+                continue;
+            }
+            
+            run_process(command, args);
+        }
+    }
+    
+    return 0;
 }
