409454e3a4fcb84ba9d6fa40b9231b0a8540bedc
Nikita Kartashov <snailandmail@gmail.com>
2014-04-13 12:41:44 +0400

    Added allocator

---
+++ labs/2014/kartashov.nikita/4/src/block.h
@@ -0,0 +1,57 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+class block;
+
+const int INT_SIZE = 4;
+const int HEADER_SIZE = 3 * INT_SIZE;
+block* NO_BLOCK = 0;
+const int MIN_BLOCK_SIZE = HEADER_SIZE + 1;
+
+class block
+{
+public:
+	block(int size) : 
+		m_size(size), 
+		m_next(NO_BLOCK), 
+		m_prev(NO_BLOCK) {}
+
+	int available() {return is_free() ? m_size + HEADER_SIZE : 0;}
+	bool is_free() {return m_size > 0;}
+	void allocate() {m_size *= -1;}
+	void free() {allocate();} // xDDDDDDD
+	int real_size() {return abs(m_size);}
+	void merge()
+	{
+		if (!is_free() || m_next == NO_BLOCK || !m_next->is_free())
+			return;
+	
+		block* ptr = m_next;
+		if (m_next->m_next != NO_BLOCK)
+			m_next->m_next->m_prev = this;
+		m_next = m_next->m_next;
+		m_size += HEADER_SIZE + ptr->m_size;
+		delete ptr;
+	}
+
+	void prev_merge()
+	{
+		if (m_prev == NO_BLOCK)
+			return;		
+		m_prev->merge();
+	}
+
+	block* next() {return m_next;}
+	block* prev() {return m_prev;}
+	void set_prev(block* prev) {m_prev = prev;}
+	void set_next(block* next) {m_next = next;}
+	int size() {return m_size;}
+	void set_size(int size) {m_size = size;}
+
+private:
+	int m_size;
+	block* m_next;
+	block* m_prev;
+};
+
+#endif /* end of include guard */
