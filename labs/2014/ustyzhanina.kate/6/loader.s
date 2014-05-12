db9db8362ce951304facc6aa8debc170bcae99f0
katty <katty@katty.(none)>
2014-05-12 15:02:55 +0400

    loader ustuzhanina

---
+++ labs/2014/ustyzhanina.kate/6/loader.s
@@ -0,0 +1,34 @@
+.code16
+.text
+.globl _start;
+
+_start:
+	cli
+	movw %cs,     %ax
+	movw %ax,     %ds
+	addw $0x0220, %ax
+	movw %ax,     %ss
+	movw $0x0100, %sp
+	sti
+
+	leaw  message, %si
+	jmp .print
+
+	.print:
+		lodsb
+		orb  %al, %al
+		jz   .printdone
+		movb $0x0e, %ah
+		int  $0x10
+		jmp  .print
+
+	.printdone:
+		ret
+
+
+message:
+	.ascii "Hello, World!\n\r"  
+
+	. = _start + 510
+	    .byte 0x55
+	    .byte 0xaa
