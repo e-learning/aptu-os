7a055e92d003f8d6ccd75969eb08233e9fbed1d2
satamas <se.atamas@gmail.com>
2014-05-13 12:47:03 +0400

    Export fix

--- labs/2014/atamas.semyon/3/fs.cpp
+++ labs/2014/atamas.semyon/3/fs.cpp
@@ -175,6 +175,7 @@ void FS::copy(FileDescriptor src_file, FileDescriptor & dst_fold, std::string ds
         file.set_filename(src_file.filename);
     }
 	file.parent_file = dst_fold.first_block;
+	file.size = src_file.size;
 
 	if(dst_fold.first_child != -1){
 		Block * nb = new Block(dst_fold.first_child, config.block_size, _root);
