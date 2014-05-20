dbd8d5f2d331e8753819e2a450a5b30f74c77dc4
Krasavin93 <Krasavin93@gmail.com>
2014-05-20 07:09:41 +0000

    ispravleno

--- labs/1142/Krasavin.Anatoliy/2module/alloc.cpp
+++ labs/1142/Krasavin.Anatoliy/2module/alloc.cpp
@@ -1,43 +1,41 @@
-#include <stdio.h>
+##include <stdio.h>
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main(void)
 {
-	cout << "Blocks Space=";
-	int N, block, size;
-	cin >> N;
-	if ((N<100) && (N>10000)){
-		cout<<"Error! Input correct \n";
-
-	}
-	string command;
-	while (1)
-	{
-		cout << "./";
-		cin >> command;
-		if (command == "exit") break;
-		if (command == "info"){
-			cout << "FreeBlock:" << N << "; " << endl;
-			cout << "SumBlocks:" << block << "; " << endl;
-			cout << "UsedSpace:" << size << "; " << endl;
-		}
-		if (command == "alloc"){
-			int i;
-			cout << "Size:";
-			cin >> i;
-			N -= i;
-			if (N<0){
-				cout << " - " << endl;
-				N += i;
-			}
-			else
-			{
-				block++;
-				size += i;
-				cout << " + " << size << endl;
-			}
-		}
-	}
+        cout<<"N block=";
+        int N, block, size;
+        cin>>N;
+        if((N<100)&&(N>10000)){
+        cout<<"ERR! \n";
+        }
+        string command;
+        while(1)
+        {
+        cout<<":";
+        cin>>command;
+        if (command=="exit") break;     
+        if (command=="info"){
+        cout<<"FreeBlock:"<< N <<"; "<<endl;
+        cout<<"SumBlocks:"<< block <<"; "<<endl;
+        cout<<"UsedSpace:"<< size <<"; "<<endl;
+        } 
+        if (command=="alloc"){   
+                int i;
+                cout<<"Size:";
+                cin>>i;
+                N-=i;      
+                if (N<0){
+                cout<< " - "<<endl;
+                N+=i;
+                        }
+                else
+                        {                       
+                        block ++;     
+                        size +=i;
+                        cout<<" + "<<size<<endl;
+                        }
+                }
+        }
 }
