d176e8c70ffe9731be33fbeb19da570e48f622d1
iVladik94 <iVladik94@gmail.com>
2014-05-27 06:30:02 +0000

    done

--- labs/1142/Falzan.Vladislav/5/client.cpp
+++ labs/1142/Falzan.Vladislav/5/client.cpp
@@ -1,4 +1,3 @@
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -8,19 +7,18 @@
 #include <pthread.h>
 
 #define PORT 3425
-#define size 100
-
 bool flag = false;
 using namespace std;
-void *rec(void* arg)
+void *reciever(void* arg)
 {
-        char buf[size];
-        int *sock;
-        sock = (int*)arg;
+        char mes[255];
+        int *socket;
+        socket = (int*)arg;
         while(1)
         {
-                recv(*sock,buf,size,0);
-                cout<<buf<<endl;
+                recv(*socket,mes,255,0);
+                cout<<mes<<endl;
+                sleep(1);
         }
         return NULL;
 }
@@ -33,54 +31,48 @@ void error(const char *msg)
 }
 
 int main()
-{       pthread_t thread;
-        struct sockaddr_in addr;
+{       pthread_t thr_handler;
+        struct sockaddr_in adr_st;
         struct in_addr in_ad;
         string message;
-        string name;
+        string user_name;
         bool run=true;
-        int soc;
+        int socket_h;
 
         do
         {
-                name.clear();
+                user_name.clear();
                 cout<<"What is your name?(max 25 chr): ";
-                cin>>name;
+                cin>>user_name;
         }
-        while(name.length()>25);
+        while(user_name.length()>25);
 
-        //create socket
-        soc=socket(AF_INET,SOCK_STREAM,0); 
-        if (soc<0)
+        socket_h=socket(AF_INET,SOCK_STREAM,0);
+        if (socket_h<0)
                 error("Can't create socket");
-        //struct filling
         in_ad.s_addr=inet_addr("127.0.0.1");
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(PORT); 
-        addr.sin_addr = in_ad;
-        //connect to server
-        if(connect (soc,(struct sockaddr *)&addr,sizeof(addr))<0)
+        adr_st.sin_family = AF_INET;
+        adr_st.sin_port = htons(PORT);
+        adr_st.sin_addr = in_ad;
+        if(connect (socket_h,(struct sockaddr *)&adr_st,sizeof(adr_st))<0)
                 error("Can't connect!");
-        cout<<"Connected!\n";
-        cout<<"You can send messages\n";       
-        pthread_create(&thread,NULL,rec,(void*)&soc);
+        cout<<"Connected!"<<endl<<"STOP to close programm"<<endl;
+        pthread_create(&thr_handler,NULL,reciever,(void*)&socket_h);
         while (1)
         {      
                 string work;
                 getline(cin,work);
-                message=name+":"+work;
+                message=user_name+":"+work;
                
-                if(send(soc,message.c_str(),message.length(),0)<0)
+                if(send(socket_h,message.c_str(),message.length(),0)<0)
                                 error("Can't send!");
                 message.clear();
-                if (work=="STOP_CHAT")
+                if (work=="STOP")
                         break;
                 work.clear();
 
                
         }
-        close(soc);
+        close(socket_h);
         return 0;
 }
-
-
