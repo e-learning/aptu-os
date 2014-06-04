dee7f9f1c85078a3980eac349de18b30748556e2
antonfait17 <antonfait17@gmail.com>
2014-06-04 20:19:56 +0000

    fix

--- labs/2014/Kryshchenko.Anton/6/boot_loader.s
+++ labs/2014/Kryshchenko.Anton/6/boot_loader.s
@@ -6,25 +6,23 @@ _start:
         cli
         movw %cs, %ax
         movw %ax, %ds
-        addw $0x0220, %ax
         movw %ax, %ss
         movw $0x7C00, %sp
         sti
-
-helloworld_txt: .ascii "hello, world"
-.set len, .-helloworld_txt
-
-        lea helloworld_txt, %si
-        movb $len, %cl
-
+		
+		leaw helloworld_txt, %si
+		movw $len, %cx
 Print:
         lodsb
         movb $0x0E, %ah
         int  $0x10
-        loop Print       
+        loop Print
         hlt
-    
+
+helloworld_txt:
+	.ascii "Hello world!\n"
+	.set len, .-helloworld_txt
 
 . = _start + 510
 .byte 0x55
-.byte 0xAA
+.byte 0xaa
