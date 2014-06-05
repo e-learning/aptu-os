e49a0b7918e53b0b4f14ca7f76e795f7b8f53b20
Bubnov.Nikita <dopehate@gmail.com>
2014-06-05 23:42:34 +0400

    changes made

--- labs/2014/bubnov.nikita/6/bootloader.s
+++ labs/2014/bubnov.nikita/6/bootloader.s
@@ -3,23 +3,32 @@
 .text
 .globl _start
 _start:
-     mov %al, 0X01
-     mov %ah, 0X00
-     int 0x10
+
+
+     lea %si, message
+     mov %bp, %si
+
+     mov %cx, 0
+myloop:
+     inc %si
+     inc %cx
+     mov %ax, [%si]
+     cmp %ax, 0
+     jne myloop
+
      mov %ax, 0
      mov %es, %ax
-     mov %bp, OFFSET FLAT:message
+     
      mov %ah, 0x13
      mov %bh, 0
      mov %bl, 0x07
-     mov %cx, 12
      mov %dh, 0
      mov %dl, 0
      int 0x10
      jmp $
 
 message:
-     .ascii "hello world!"
+     .asciz "hello world!"
      . = _start + 510
      .byte 0x55
-     .byte 0xaa	
+     .byte 0xaa
