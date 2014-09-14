95f8d66ec4437781ac5858d4727d3e6e23eb09be
Andrey Lazarevich <thecexcell@gmail.com>
2014-09-14 13:26:13 +0400

    ls and pwd are ready

--- labs/504/lazarevich.andrey/01CommandInterpreter/ls.c
+++ labs/504/lazarevich.andrey/01CommandInterpreter/ls.c
@@ -1,6 +1,42 @@
 #include "common.h"
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <dirent.h>
+#include <string.h>
 
 void ls_run()
 {
-	printf("LS\n");
+	char 			files[256][256];
+	DIR 			*dir;
+	struct dirent	*entry;	
+	struct winsize	w;
+	int				counter = 0;
+	size_t 			length = 0;
+	
+	ioctl(0, TIOCGWINSZ, &w);
+	dir = opendir("./");
+	if (!dir)
+	{
+		perror("opendir");
+		exit(1);
+	}
+	
+	while (	(entry = readdir(dir)) != NULL )
+	{
+		if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+		{ 
+			strcpy( files[counter], entry->d_name);
+			if ((length + strlen(files[counter])) > w.ws_col)
+			{
+				length = 0;
+				printf("\n%s\t", files[counter]);
+			}
+			else
+				printf("%s\t", files[counter]);
+
+			length += strlen(files[counter]) + 4;		
+			counter++;
+		}
+	}
+	printf("\nTotal number of files: %d\n", counter);	
 }
--- labs/504/lazarevich.andrey/01CommandInterpreter/pwd.c
+++ labs/504/lazarevich.andrey/01CommandInterpreter/pwd.c
@@ -1,6 +1,14 @@
 #include "common.h"
+#define MAX_PATH_SIZE 256
 
 void pwd_run()
 {
-	printf("PWD\n");
+	char pwd[MAX_PATH_SIZE];
+	if (getcwd(pwd, MAX_PATH_SIZE) == NULL)
+	{
+		perror("getcwd");
+		exit(1);
+	}
+	else
+		printf("%s\n", pwd);
 }
