9b3871f52bc2f8fb9f107d45ada63c3ce8569c3b
Semyon Proshev <s-proshev@ya.ru>
2014-10-13 12:21:12 +0400

    Proshev Semyon: 03-allocator, reimplement

--- labs/504/proshev.semyon/03-allocator/main.cpp
+++ labs/504/proshev.semyon/03-allocator/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <stddef.h>
 
 struct MCB
 {
@@ -15,11 +14,11 @@ class Allocator
 
 private:
     char* buffer_;
-    size_t size_;
+    const size_t size_;
     MCB* root_;
 
 public:
-    Allocator(size_t size) : buffer_(new char[size]), size_(size) {
+    Allocator(const size_t size) : buffer_(new char[size]), size_(size) {
         root_ = (MCB*) buffer_;
 
         root_->size = size;
@@ -34,107 +33,52 @@ public:
 
     void my_alloc(const size_t desired_size)
     {
-        if (desired_size > size_ - sizeof(MCB))
+        MCB* available_mcb = find_available_mcb(desired_size);
+
+        if (available_mcb == nullptr)
         {
             std::cout << "-" << std::endl;
             return;
         }
 
-        MCB* current = root_;
-
-        while (current->used || current->size < desired_size + sizeof(MCB))
+        if (available_mcb->next == nullptr)
         {
-            current = current->next;
-
-            if (current == nullptr)
-            {
-                std::cout << "-" << std::endl;
-                return;
-            }
-        }
-
-        if ((char*) (current + 2) > buffer_ + size_ - desired_size)
-        {
-            current->next = nullptr;
+            insert_new_last_mcb_if_possible(available_mcb, desired_size);
         }
         else
         {
-            MCB* next = (MCB*) ((char*) (current + 1) + desired_size);
-
-            if (current-> next != next) {
-                next->size = current->size - desired_size - sizeof(MCB);
-                next->used = false;
-                next->prev = current;
-                next->next = current->next;
-
-                if (current->next != nullptr) {
-                    current->next->prev = next;
-                }
-
-                current->next = next;
-            }
+            insert_new_mcb_if_possible(available_mcb, desired_size);
         }
 
-        current->size = desired_size + sizeof(MCB);
-        current->used = true;
+        available_mcb->used = true;
+        available_mcb->size = desired_size + sizeof(MCB);
 
-        std::cout << "+" << " " << (char*) (current + 1) - (char*) root_ << std::endl;
+        std::cout << "+" << " " << (char*) (available_mcb + 1) - (char*) root_ << std::endl;
     }
 
     void my_free(const size_t offset)
     {
-        if (offset > size_)
+        MCB* mcb = find_mcb_by_offset(offset);
+
+        if (mcb == nullptr || !mcb->used)
         {
             std::cout << "-" << std::endl;
             return;
         }
 
-        MCB* current = root_;
-        char* pointer = buffer_ + offset;
+        mcb->used = false;
 
-        while (pointer != (char*) (current + 1))
+        if (mcb->next == nullptr)
         {
-            current = current->next;
-
-            if (current == nullptr)
-            {
-                std::cout << "-" << std::endl;
-                return;
-            }
+            mcb->size = buffer_ + size_ - (char*) mcb;
+        }
+        else
+        {
+            mcb->size = (char*) mcb->next - (char*) mcb;
         }
 
-        current->used = false;
-
-        if (current->next == nullptr && (char*) (current + 1) + current->size != buffer_ + size_)
-        {
-            current->size += buffer_ + size_ - (char*) (current) - current->size;
-        }
-
-        if (current->next != nullptr && !current->next->used)
-        {
-            MCB* next = current->next;
-
-            current->size += next->size;
-            current->next = next->next;
-
-            if (next->next != nullptr)
-            {
-                next->next->prev = current;
-            }
-        }
-
-        if (current->prev != nullptr && !current->prev->used)
-        {
-            MCB* prev = current->prev;
-
-            prev->size += current->size;
-            prev->next = current->next;
-
-            if (current->next != nullptr)
-            {
-                current->next->prev = prev;
-            }
-        }
+        merge_mcb_and_next_if_possible(mcb);
+        merge_mcb_and_next_if_possible(mcb->prev);
 
         std::cout << "+" << std::endl;
     }
@@ -171,7 +115,7 @@ public:
     {
         MCB* current = root_;
 
-        while (1)
+        while (current != nullptr)
         {
             for (size_t i = 0; i < 32; ++i)
             {
@@ -183,6 +127,8 @@ public:
                 {
                     std::cout << "u";
                 }
+
+                print_f_if_needed(current);
             }
             else
             {
@@ -192,20 +138,111 @@ public:
                 }
             }
 
-            if (current->next == nullptr)
-            {
-                for (size_t i = 0; i < buffer_ + size_ - (char*) (current) - current->size; ++i)
-                {
-                    std::cout << "f";
-                }
-
-                break;
-            }
-
             current = current->next;
         }
 
         std::cout << std::endl;
+    }
+
+    void print_f_if_needed(const MCB* current) {
+        if (current->next == nullptr)
+        {
+            size_t space_before_end = buffer_ + size_ - (char*) (current) - current->size;
+
+            for (size_t i = 0; i < space_before_end; ++i)
+            {
+                std::cout << "f";
+            }
+        }
+        else
+        {
+            size_t space_before_next = (char*) current->next - (char*) current - current->size;
+
+            for (size_t i = 0; i < space_before_next; ++i)
+            {
+                std::cout << "f";
+            }
+        }
+    }
+
+private:
+    MCB* find_available_mcb(const size_t desired_size)
+    {
+        MCB* mcb = root_;
+
+        while (mcb != nullptr && (mcb->used || mcb->size < desired_size + sizeof(MCB)))
+        {
+            mcb = mcb->next;
+        }
+
+        return mcb;
+    }
+
+    void insert_new_last_mcb_if_possible(MCB* last_mcb, const size_t size) {
+        char* new_last_ptr = (char*) (last_mcb + 1) + size;
+
+        if (new_last_ptr <= buffer_ + size_ - sizeof(MCB))
+        {
+            MCB* new_last_mcb = (MCB*) new_last_ptr;
+
+            new_last_mcb->size = last_mcb->size - size - sizeof(MCB);
+            new_last_mcb->used = false;
+            new_last_mcb->prev = last_mcb;
+            new_last_mcb->next = nullptr;
+
+            last_mcb->next = new_last_mcb;
+        }
+    }
+
+    void insert_new_mcb_if_possible(MCB* mcb, const size_t size) {
+        char* new_ptr = (char*) (mcb + 1) + size;
+
+        if (new_ptr != (char*) mcb->next && new_ptr + sizeof(MCB) <= (char*) mcb->next)
+        {
+            MCB* new_mcb = (MCB*) new_ptr;
+
+            new_mcb->size = mcb->size - size - sizeof(MCB);
+            new_mcb->used = false;
+            new_mcb->prev = mcb;
+            new_mcb->next = mcb->next;
+
+            mcb->next->prev = new_mcb;
+            mcb->next = new_mcb;
+        }
+    }
+
+    MCB* find_mcb_by_offset(const size_t offset)
+    {
+        MCB* mcb = root_;
+
+        while (mcb != nullptr && buffer_ + offset != (char*) (mcb + 1))
+        {
+            mcb = mcb->next;
+        }
+
+        return mcb;
+    }
+
+    void merge_mcb_and_next_if_possible(MCB *mcb)
+    {
+        if (mcb == nullptr || mcb->next == nullptr || mcb->next->used)
+        {
+            return;
+        }
+
+        MCB* next = mcb->next;
+
+        mcb->next = next->next;
+
+        if (mcb->next == nullptr)
+        {
+            mcb->size = buffer_ + size_ - (char*) mcb;
+        }
+        else
+        {
+            mcb->next->prev = mcb;
+            mcb->size = (char*) mcb->next - (char*) mcb;
+        }
     }
 
 };
