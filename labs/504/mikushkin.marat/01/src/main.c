e701d9b6e8cf096bad34188675286f75b5b8a1c6
mma <marat.mikushkin@gmail.com>
2014-10-19 20:29:04 +0400

    some changes

--- labs/504/mikushkin.marat/01/src/main.c
+++ labs/504/mikushkin.marat/01/src/main.c
@@ -4,27 +4,35 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <signal.h>
 
-const int MAX_USER_COMMAND_LENGTH = 256;
+#define MAX_USER_COMMAND_LEN 100
+#define EXEC_SUCCESS 0
+#define EXEC_FAILURE -1
+#define MAX_LEN_PROC_PATH 100
+#define MAX_LEN_PROC_NAME 100
 
-int parseInput(char ** argv) {
-	char userInput[MAX_USER_COMMAND_LENGTH];
+char * get_current_dir_name();
 
-	if (fgets(userInput, MAX_USER_COMMAND_LENGTH, stdin) == NULL) {
-		return -1;
+int parse_input(char ** argv) {
+	char user_input[MAX_USER_COMMAND_LEN];
+
+	if (fgets(user_input, MAX_USER_COMMAND_LEN, stdin) == NULL) {
+		return EXEC_FAILURE;
 	}
 
-	if (strcmp(userInput, "\n") == 0) {
-		return 0;
+	if (strcmp(user_input, "\n") == 0) {
+		return EXEC_SUCCESS;
 	}
 
-	char * token = strtok(userInput, " ");
+	char * token = strtok(user_input, " ");
 	int argc = 0;
 	for (; token != NULL; token = strtok(NULL, " ")) {
 		if (token[strlen(token) - 1] == '\n') {
 			token[strlen(token) - 1] = '\0';
 		}
-		argv[argc] = (char *) malloc (strlen(token) + 1);
+		argv[argc] = (char *) malloc(strlen(token) + 1);
 		strcpy(argv[argc++], token);
 		if (strcmp(argv[argc - 1], "exit") == 0) {
 			exit(0);
@@ -34,16 +42,16 @@ int parseInput(char ** argv) {
 	return argc;
 }
 
-void execute(int argc, char** argv) {
+void execute(int argc, char ** argv) {
 	pid_t pid;
-	int foundAmpersand = (strcmp(argv[argc - 1], "&") == 0);
+	int found_ampersand = (strcmp(argv[argc - 1], "&") == 0);
 
 	pid = fork();
 
 	if (pid < 0) {
-		perror("Error (pid < 0)");
+		perror("Error: pid < 0");
 	} else if (pid == 0) {
-		if (foundAmpersand) {
+		if (found_ampersand) {
 			argv[argc - 1] = NULL;
 			argc--;
 		}
@@ -52,19 +60,95 @@ void execute(int argc, char** argv) {
 			exit(0);
 		}
 
-	} else if (!foundAmpersand) {
+	} else if (!found_ampersand) {
 		waitpid(pid, NULL, 0);
 	}
 }
 
+int ls() {
+	DIR * directory;
+
+	if ((directory = opendir("."))) {
+		struct dirent * content;
+
+		while ((content = readdir(directory))) {
+			if (strcmp(content->d_name, ".") != 0 && strcmp(content->d_name, "..") != 0) {
+				printf("%s\n", content->d_name);
+			}
+		}
+		closedir(directory);
+		free(content);
+	} else {
+		free(directory);
+		return EXEC_FAILURE;
+	}
+	return EXEC_SUCCESS;
+}
+
+int pwd() {
+	printf("%s\n", get_current_dir_name());
+	return EXEC_SUCCESS;
+}
+
+int ps() {
+	DIR * directory;
+
+	if ((directory = opendir("/proc"))) {
+		struct dirent * content;
+
+		while ((content = readdir(directory))) {
+			if (atoi(content->d_name)) {
+				char process_path[MAX_LEN_PROC_PATH];
+				strcpy(process_path, "/proc/");
+				strcat(process_path, content->d_name);
+				strcat(process_path, "/comm");
+
+				char process_name[MAX_LEN_PROC_NAME];
+				FILE * f = fopen(process_path, "r");
+				fgets(process_name, MAX_LEN_PROC_NAME, f);
+
+				printf("%s:  %s", content->d_name, process_name);
+
+				fclose(f);
+			}
+		}
+		free(content);
+		closedir(directory);
+	} else {
+		free(directory);
+		return EXEC_FAILURE;
+	}
+
+	return EXEC_SUCCESS;
+}
+
+int kill_by_pid(int pid) {
+	if (kill(pid, 9) == -1) {
+		return EXEC_FAILURE;
+	}
+	return EXEC_SUCCESS;
+}
+
 int main() {
-	char * partsOfUserCommand[256];
+    char * arguments[MAX_USER_COMMAND_LEN];
 
 	while (1) {
 		printf("$ ");
-		int numberOfPartsInUserCommand = parseInput(partsOfUserCommand);
-		execute(numberOfPartsInUserCommand, partsOfUserCommand);
+
+		int number_of_arguments = parse_input(arguments);
+
+		if (strcmp(arguments[0], "ls") == 0) {
+			ls();
+		} else if (strcmp(arguments[0], "ps") == 0) {
+			ps();
+		} else if (strcmp(arguments[0], "pwd") == 0) {
+			pwd();
+		} else if (strcmp(arguments[0], "kill") == 0) {
+			kill_by_pid(atoi(arguments[0]));
+		} else {
+			execute(number_of_arguments, arguments);
+		}
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
