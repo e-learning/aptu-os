aedea571012b63bea70ae6c972bb3a195bb2206c
mma <marat.mikushkin@gmail.com>
2014-11-10 23:21:22 +0300

    some changes

--- labs/504/mikushkin.marat/06/06.cpp
+++ labs/504/mikushkin.marat/06/06.cpp
@@ -1,9 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <vector>
+#include <iostream>
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/time.h>
-#include <vector>
 
 using namespace std;
 
@@ -37,52 +36,36 @@ void *zero_multiples(void *threadid) {
 
 int main(int argc, char *argv[]) {
 	vector<pthread_t> threads;
-	int rc;
 
-	size_t is_all_thread_done = 0;
-	int number_of_active_threads = 0;
-
-	size_t number_of_threads = atoi(argv[2]);
 	numbers.resize(atoi(argv[1]));
+	threads.resize(atoi(argv[2]));
 
 	for (size_t i = 1; i < numbers.size(); i++) {
 		numbers[i] = i + 1;
 	} /* start at index 1 so that number 1 starts zeroed out */
 
-	threads.resize(number_of_threads);
 
-	thread_is_done.resize(number_of_threads);
-	thread_did_work.resize(number_of_threads);
+	thread_is_done.resize(threads.size());
+	thread_did_work.resize(threads.size());
 
-	/* create threads and run zero_multiples */
-	for (size_t i = 0; i < number_of_threads; i++) {
-		//if(debug_level>1) printf("Creating thread %d\n", i);
-		rc = pthread_create(&threads[i], NULL, zero_multiples, (void *) (intptr_t) i); /* create thread to run zero_multiples() */
-		if (rc) {
-			printf("ERROR; return code from pthread_create() is %d\n", rc);
-			exit(-1);
-		}
+	for (size_t i = 0; i < threads.size(); i++) {
+		pthread_create(&threads[i], NULL, zero_multiples, (void *) (intptr_t) i);
 	}
 
 	/* main program wait until all threads are complete */
-	while (is_all_thread_done < number_of_threads) /* exit only when all threads have set their done_sw */
+	size_t is_all_thread_done = 0;
+	while (is_all_thread_done < threads.size()) /* exit only when all threads have set their done_sw */
 	{
 		is_all_thread_done = 0;
-		for (size_t i = 0; i < number_of_threads; i++) {
+		for (size_t i = 0; i < threads.size(); i++) {
 			is_all_thread_done = is_all_thread_done + thread_is_done[i]; /* count how many threads are done */
 		}
-	}
-
-	/* count number of threads that did work */
-	number_of_active_threads = 0;
-	for (size_t i = 0; i < number_of_threads; i++) {
-		number_of_active_threads = number_of_active_threads + thread_did_work[i];
 	}
 
 	if (true) {
 		for (size_t k = 0; k < numbers.size(); k++) {
 			if (numbers[k] != 0) {
-				printf("%d\n", abs(numbers[k]));
+				cout << abs(numbers[k]) << endl;
 			}
 		}
 	}
