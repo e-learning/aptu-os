9bdc10c6d9c8cb055695bfc8f6e069e7851e34f4
mma <marat.mikushkin@gmail.com>
2014-11-11 00:09:22 +0300

    some changes

--- labs/504/mikushkin.marat/06/sieve_multith.cpp
+++ labs/504/mikushkin.marat/06/sieve_multith.cpp
@@ -19,12 +19,10 @@ void *zero_multiples(void *threadid) {
 		prime = numbers[i];
 		prime_ind = i;
 
-		if (prime > 0)
-		{
+		if (prime > 0) {
 			thread_did_work[(intptr_t) threadid] = 1;
 			numbers[prime_ind] = -numbers[prime_ind];
-			for (size_t k = prime_ind + prime; k < numbers.size(); k = k + prime)
-			{
+			for (size_t k = prime_ind + prime; k < numbers.size(); k = k + prime) {
 				numbers[k] = 0;
 			}
 		}
@@ -36,12 +34,12 @@ void *zero_multiples(void *threadid) {
 }
 
 void print_usage() {
-    cout << "usage:" << endl;
-    cout << "./sieve_multith N M -p" << endl;
-    cout << "where:" << endl;
-    cout << " N <-- sieve limit" << endl;
-    cout << " M <-- number of threads" << endl;
-    cout << "-p <-- print finded primes, optional parameter" << endl;
+	cout << "usage:" << endl;
+	cout << "./sieve_multith -p N M" << endl;
+	cout << "where:" << endl;
+	cout << "-p <-- print finded primes, optional parameter" << endl;
+	cout << " N <-- sieve limit" << endl;
+	cout << " M <-- number of threads" << endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -52,13 +50,17 @@ int main(int argc, char *argv[]) {
 
 	vector<pthread_t> threads;
 
-	numbers.resize(atoi(argv[1]));
-	threads.resize(atoi(argv[2]));
+	if (string(argv[1]) == "-p") {
+		numbers.resize(atoi(argv[2]));
+		threads.resize(atoi(argv[3]));
+	} else {
+		numbers.resize(atoi(argv[1]));
+		threads.resize(atoi(argv[2]));
+	}
 
 	for (size_t i = 1; i < numbers.size(); i++) {
 		numbers[i] = i + 1;
 	}
-
 
 	thread_is_done.resize(threads.size());
 	thread_did_work.resize(threads.size());
@@ -68,12 +70,11 @@ int main(int argc, char *argv[]) {
 	}
 
 	size_t is_all_thread_done = 0;
-	while (is_all_thread_done < threads.size())
-	{
+	while (is_all_thread_done < threads.size()) {
 		is_all_thread_done = accumulate(thread_is_done.begin(), thread_is_done.end(), 0);
 	}
 
-	if (argc >= 4 && string(argv[3]) == "-p") {
+	if (string(argv[1]) == "-p") {
 		for (size_t k = 0; k < numbers.size(); k++) {
 			if (numbers[k] != 0) {
 				cout << abs(numbers[k]) << endl;
