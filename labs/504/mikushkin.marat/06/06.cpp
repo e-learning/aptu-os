ded970e4735ba43943aca604842102fb065ac792
mma <marat.mikushkin@gmail.com>
2014-11-10 21:43:34 +0300

    some changes

--- labs/504/mikushkin.marat/06/06.cpp
+++ labs/504/mikushkin.marat/06/06.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/time.h>
-#include<vector>
+#include <vector>
 
 using namespace std;
 
@@ -35,39 +35,13 @@ void *zero_multiples(void *threadid) {
 	pthread_exit(NULL);
 }
 
-/* used for time elapsed */
-/* Subtract the `struct timeval' values X and Y,
- storing the result in RESULT.
- Return 1 if the difference is negative, otherwise 0.  */
-int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y) {
-	/* Perform the carry for the later subtraction by updating y. */
-	if (x->tv_usec < y->tv_usec) {
-		int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-		y->tv_usec -= 1000000 * nsec;
-		y->tv_sec += nsec;
-	}
-	if (x->tv_usec - y->tv_usec > 1000000) {
-		int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-		y->tv_usec += 1000000 * nsec;
-		y->tv_sec -= nsec;
-	}
-
-	/* Compute the time remaining to wait.
-	 tv_usec is certainly positive. */
-	result->tv_sec = x->tv_sec - y->tv_sec;
-	result->tv_usec = x->tv_usec - y->tv_usec;
-
-	/* Return 1 if result is negative. */
-	return x->tv_sec < y->tv_sec;
-}
-
 int main(int argc, char *argv[]) {
 	vector<pthread_t> threads;
 	int rc;
 
 	struct timeval tv_1;
 	struct timeval tv_2;
-	struct timeval result;
+	//struct timeval result;
 
 	size_t done_sw_main = 0; /* used to check if all the threads have finished */
 	int did_work_howmany = 0; /* tallies how many threads actually did work */
@@ -119,7 +93,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	gettimeofday(&tv_2, 0);
-	timeval_subtract(&result, &tv_2, &tv_1);
+	//timeval_subtract(&result, &tv_2, &tv_1);
 
 	pthread_exit(NULL);
 }
