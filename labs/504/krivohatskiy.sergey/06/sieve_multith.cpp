71260b6dfcf584436e4d75c98666181dcdd42471
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2014-11-29 19:38:40 +0300

    06 task fixed
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- labs/504/krivohatskiy.sergey/06/sieve_multith.cpp
+++ labs/504/krivohatskiy.sergey/06/sieve_multith.cpp
@@ -4,13 +4,27 @@
 #include <time.h>
 #include <cstring>
 #include <stdlib.h>
+#include <atomic>
 
-void thread_function(bool *arr, uint64_t from, uint64_t to, uint64_t step)
+void thread_function(bool *arr, std::atomic_uint_least64_t &index, u_int64_t n)
 {
-	for (uint64_t k = from; k <= to; k += step)
-	{
-		arr[k] = false;
-	}
+    while(index < n)
+    {
+        u_int64_t i = index.fetch_add(1);
+        if(!arr[i])
+        {
+            continue;
+        }
+        uint64_t k = i * i;
+        if(k > n)
+        {
+            return;
+        }
+        for (; k <= n; k += i)
+        {
+            arr[k] = false;
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -40,37 +54,25 @@ int main(int argc, char *argv[])
 	bool *arr = new bool[(size_t)n + 1];
 	memset(arr, true, sizeof(bool)* ((size_t)n + 1));
     std::thread *threads = new std::thread[m];
+    std::atomic_uint_least64_t index(2);
 
-	for (uint64_t i = 2; ((i*i) <= n); ++i)
-	{
-		if (!arr[i])
-		{
-			continue;
-		}
-		uint64_t from = i*i;
-		uint64_t step = ((n - from + i - 1) / i / m) * i;
-		for (int j = 0; j < m - 1; ++j)
-		{
-			// TODO reuse threads(thread pool or something)
-			threads[j] = std::thread(thread_function, arr, from, from + step - 1, i);
-			from += step;
-		}
-		threads[m - 1] = std::thread(thread_function, arr, from, n, i);
-
-		for (int j = 0; j < m; ++j)
-		{
-			threads[j].join();
-		}
-	}
+    for(int i = 0; i < m; ++i)
+    {
+        threads[i] = std::thread(thread_function, arr, std::ref(index), n);
+    }
+    for(int i = 0; i < m; ++i)
+    {
+        threads[i].join();
+    }
     delete[] threads;
 
 	if (!print)
     {
 		delete[] arr;
-		return 0;
+        return 0;
 	}
 
-	for (uint64_t i = 1; i <= n; ++i)
+    for (uint64_t i = 1; i <= n; ++i)
 	{
 		if (arr[i])
 		{
