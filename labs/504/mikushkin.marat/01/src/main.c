9c9a24da87adc7fc0e4eeb6614573c3a992b3164
mma <marat.mikushkin@gmail.com>
2014-12-01 22:41:52 +0300

    some changes

--- labs/504/mikushkin.marat/01/src/main.c
+++ labs/504/mikushkin.marat/01/src/main.c
@@ -1,9 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <signal.h>
 
 #include "constants.h"
@@ -27,5 +23,5 @@ int main() {
 		}
 	}
 
-	return EXIT_SUCCESS;
+	return 0;
 }
