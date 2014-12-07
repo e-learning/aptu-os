36a02de1810972e1dca6827cae36c003eb33e89a
Dmitry Tishchenko <flir3d@gmail.com>
2014-12-07 22:22:56 +0300

    Tishchenko 06 Sieve

---
+++ labs/504/tishchenko.dmitry/06_sieve/experiments.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+__author__ = 'Dmitry V. Tishchenko'
+import optparse
+import argparse
+import sys
+import time
+import subprocess
+
+multprog = "./sieve_multith"
+uniprog = "./sieve_unith"
+
+def perform_iters(progname, argsline, iterations):
+    args = [progname,]
+    args.extend(argsline)
+    exectime = 0
+    for i in range(iterations):
+        start = time.time()
+        subprocess.call(args)
+        end = time.time()
+        exectime += end - start
+    exectime/= iterations
+    return exectime
+
+def findopt(iterations, outfile):
+    #M; N; time M[1,20]
+    outformat = "{:d}; {:d}; {:.3f}\n"
+    test_intervals = (100000, 500000, 1000000)
+
+    with open(outfile, 'w') as out:
+        for interval in test_intervals:
+            result = perform_iters(uniprog, [str(interval),], iterations)
+            out.write(str.format(outformat, 1, interval, result*1000))
+        for threads_count in range(2,21):
+            for interval in test_intervals:
+                result = perform_iters(multprog, [str(threads_count), str(interval)], iterations)
+                out.write(str.format(outformat, threads_count, interval, result*1000))
+
+def compare(iterations, outfile):
+    outformat = "{:s}; {:d}; {:.3f}\n"
+    with open(outfile, 'w') as out:
+        for interval in range(10000, 1000001, 10000):
+            uniresult = perform_iters(uniprog, [str(interval),], iterations)
+            out.write(str.format(outformat, "UNI", interval, uniresult*1000))
+            multresult = perform_iters(multprog, ["1", str(interval)], iterations)
+            out.write(str.format(outformat, "MULT", interval, multresult*1000))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="experiments")
+    subparsers = parser.add_subparsers(help='subcommands:', dest="utility")
+
+    parser_findopt = subparsers.add_parser('findopt', help='find optimal threads count for this machine')
+    parser_findopt.add_argument("--out", dest="outfile", help="output filename")
+    parser_findopt.add_argument('-i', type=int, default=3, dest="iterations", help='iterations for experiment measurement')
+
+    parser_report1 = subparsers.add_parser('compare', help='compares unithreaded and multithreaded programs')
+    parser_report1.add_argument("--out", dest="outfile", help="output filename")
+    parser_report1.add_argument('-i', type=int, default=3, dest="iterations", help='iterations for experiment measurement')
+
+    args = parser.parse_args()
+    if args.utility == "findopt":
+        findopt(args.iterations,args.outfile)
+    elif args.utility == "compare":
+        compare(args.iterations,args.outfile)
+
+
+
---
+++ labs/504/tishchenko.dmitry/06_sieve/sieve_unith.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PROGRAMMNAME "sieve_unith"
+
+using namespace std;
+
+void runEratosthenesSieve(int upperBound, bool *isComposite) {
+    int upperBoundSquareRoot = (int)sqrt((double)upperBound);
+    for (int m = 2; m <= upperBoundSquareRoot; m++) {
+        if (!isComposite[m]) {
+            for (int k = m * m; k < upperBound; k += m)
+                isComposite[k] = true;
+        }
+    }
+}
+
+void print_using()
+{
+    cout << "Using: " <<PROGRAMMNAME <<"[-p] number" << endl << \
+            "-p: print out found numbers" << endl << \
+            "number: upper bound for search interval" << endl;
+}
+
+bool parse_args(int argc, char **argv, int &upper_bound, bool &print_needed) {
+    //format: programmname [-p] number
+    char *nptr, *endptr;
+    if (argc == 2) //without print
+    {
+        print_needed = false;
+        nptr = argv[1];
+    }
+    else if (argc == 3) //with print
+    {
+        if(strcmp(argv[1], "-p")!=0)
+        {
+            return false; //incorrect arg instead of -p
+        }
+        print_needed = true;
+        nptr = argv[2];
+    }
+    else {
+        return false; //not enough or too many args
+    }
+    endptr = nptr;
+    upper_bound = strtol(nptr, &endptr, 10);
+    if ((endptr == nptr) || (*endptr != '\0')) {
+        return false; //not integer
+    }
+    return true;
+}
+
+int main(int argc, char **argv) {
+    int upper_bound;
+    bool print_needed;
+    if(!parse_args(argc, argv, upper_bound, print_needed))
+    {
+        print_using();
+        return EXIT_FAILURE;
+    }
+    bool *isComposite = (bool*)calloc(upper_bound, sizeof(bool));
+    runEratosthenesSieve(upper_bound, isComposite);
+    if(print_needed)
+    {
+        for (int number = 2; number < upper_bound; number++)
+        {
+            if (!isComposite[number])
+            {
+                cout << number << endl;
+            }
+        }
+    }
+    delete[] isComposite;
+    return 0;
+}
